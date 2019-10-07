a21a7525b654f650e15a6364be048a2ae2c4a5e2
Sam <samhardyhey@gmail.com>
2019-10-07 15:09:29 +1000

    tidy, format comments

--- tmux/tmux_cpu_notebook_dev.sh
+++ tmux/tmux_cpu_notebook_dev.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-# new session, called dev, with  the first window named watch
+# new session, called tmux_cpu_notebook_dev, with the first window named watch_notebook_server
 tmux new-session -s 'tmux_cpu_notebook_dev' -n 'watch_notebook_server' -d
 
-# htop within the top pane
+# htop within the left pane
 tmux send-keys -t tmux_cpu_notebook_dev:watch_notebook_server 'htop' C-j
 
-# gpu watch on the lower-left
+# jupyter notebook server on the right pane
 tmux split-window -h
 tmux send-keys -t 1 'jupyter notebook' C-j
 
-# second window for bashing
+# second window with split panes for bash/whatever
 tmux new-window 'bash'
 tmux split-window -h
 
--- tmux/tmux_gpu_notebook_dev.sh
+++ tmux/tmux_gpu_notebook_dev.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-# new session, called dev, with  the first window named watch
+# new session, called tmux_gpu_notebook_dev, with  the first window named watch_notebook_server
 tmux new-session -s 'tmux_gpu_notebook_dev' -n 'watch_notebook_server' -d
 
-# htop within the top pane
+# htop within the top-left pane
 tmux send-keys -t tmux_gpu_notebook_dev:watch_notebook_server 'htop' C-j
 
-# nvidia-smi gpu watch on the lower left
+# nvidia-smi gpu watch on the lower-left pane
 tmux split-window -v
 tmux send-keys -t 1 'watch -n 0.5 nvidia-smi' C-j
 
-# notebook server on the lower-right
+# notebook server on the right
 tmux split-window -h
 tmux send-keys -t 2 'jupyter notebook' C-j
 
-# second window for bashing
+# second window for bash/whatever
 tmux new-window 'bash'
 tmux split-window -h
 
